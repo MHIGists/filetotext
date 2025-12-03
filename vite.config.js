a83bb3f551a4b2f24e1d0bb519d4851397a42727
M.Ivanov <milko.hristov.ivanov65@gmail.com>
2025-12-03 22:32:43 +0200

    Add new js files to vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/export.js', 'resources/js/processing.js', 'resources/js/progressTracker.js'],
             refresh: true,
         }),
     ],
